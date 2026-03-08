c1cc184b2e80fcda5d676e6c1bd87decf460e368
RESHMITHA R <rreshmitha22@gmail.com>
2026-03-08 20:20:53 +0530

    Add quote generator script

---
+++ Quote generator.py
@@ -0,0 +1,13 @@
+import random
+
+quotes = [
+    "Believe in yourself.",
+    "Stay hungry, stay foolish.",
+    "Dream big and dare to fail.",
+    "Success is not final."
+]
+
+quote = random.choice(quotes)
+
+print("Quote of the day:")
+print(quote)
